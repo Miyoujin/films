b3a7550203c2dbabee0a18b6cc37288853742d8c
Ruben <Ruben@MiyoujinP>
2017-03-29 20:01:24 +0200

    Se ha creado el dao de la clase user

---
+++ src/main/java/com/dbg/dao/user/UserDao.java
@@ -0,0 +1,14 @@
+package com.dbg.dao.user;
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import model.user.User;
+
+@Repository
+public interface UserDao extends PagingAndSortingRepository<User,Integer>{
+
+	public List<User> findByNameContaining(String name);
+	
+}
